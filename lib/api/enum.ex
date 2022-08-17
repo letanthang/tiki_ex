8e77db858218aad4c623506dc4bd68f6833963d1
Dung Nguyen <bluesky.1289@gmail.com>
2022-08-17 11:40:30 +0700

    Complete with testing seller api

--- lib/api/enum.ex
+++ lib/api/enum.ex
@@ -4,10 +4,24 @@ defmodule Tiki.Enums do
   # Tiki warehouse type:
   # - Pickup: Warehouse which supplies products to TIKI warehouses.
   # - Return: Warehouse which receives returned products from Tiki
-  def_enum(WarehouseTypeEnum, return: 0, pickup: 1)
+  def_enum(WarehouseType, return: 0, pickup: 1)
 
   # Tiki warehouse status
   # - inactive: 0
   # - active: 1
-  def_enum(WarehouseStatusEnum, inactive: 0, active: 1)
+  def_enum(WarehouseStatus, inactive: 0, active: 1)
+
+  # Operation model
+  # - Drop Shipping (dropship)
+  # - Fulfillment by Tiki (instock)
+  # - On-Demand Fulfillment (backorder)
+  # - Seller Delivery (seller_backorder)
+  # - Cross Border (cross_border)
+  def_enum(OperationModel, [
+    "dropship",
+    "instock",
+    "backorder",
+    "seller_backorder",
+    "cross_border"
+  ])
 end
